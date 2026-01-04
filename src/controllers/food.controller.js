bb0abe55b70782e3eef620ba4f6200fe467a91ef
swagat123og <swagatswain2004@gmail.com>
2026-01-05 00:16:08 +0530

    Next Will be Cloud

--- src/controllers/food.controller.js
+++ src/controllers/food.controller.js
@@ -1,7 +1,9 @@
 const foodModel=require('../models/food.model');
 
 async function createFood(req,res) {
-
+ console.log(req.file);
+ res.send("food item created");
+ 
     
 }
 
--- src/routes/food.routes.js
+++ src/routes/food.routes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router=express.Router();
 const authMiddleWare=require('../middleware/auth.middleware');
 const foodController = require('../controllers/food.controller');
+const multer=require('multer');
 
+const upload = multer({ 
+    storage:multer.memoryStorage()
+ })
 // (/api/food/) also for protection we used a middleware
-router.post('/',authMiddleWare.authFoodPartnerMiddleWare,foodController.createFood);
+router.post('/',authMiddleWare.authFoodPartnerMiddleWare,upload.single("video"),foodController.createFood);
 
 module.exports=router;
