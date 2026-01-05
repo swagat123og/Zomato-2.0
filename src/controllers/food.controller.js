163f645b002716bd3a2d6cd73efd9ae5ab77f1a8
swagat123og <swagatswain2004@gmail.com>
2026-01-05 21:06:18 +0530

    cloud is added succesfully

--- src/controllers/food.controller.js
+++ src/controllers/food.controller.js
@@ -1,11 +1,23 @@
 const foodModel=require('../models/food.model');
+const storageServices =require('../services/storage.service');
+const {v4:uuid}=require('uuid');
 
 async function createFood(req,res) {
- console.log(req.file);
- res.send("food item created");
- 
-    
+    const fileUloadResult = await storageServices.uploadFile(req.file.buffer,uuid());
+    const foodItem= await foodModel.create({
+      name:req.body.name,
+      description:req.body.description,
+      video:fileUloadResult.url,
+      foodPartner:req.user
+    })
+
+    res.status(201).json({
+        message:"food created succesfuly",
+        food:foodItem
+    })
 }
+
+
 
 module.exports={
     createFood
